dbbdaa72fc4302bb3f3a06c6f70da1ab3622b197
HIGH_VIVID <henry.hawgood@gmail.com>
2024-06-15 11:06:10 -0500

    alright so we're gonna be using glfw and glm, its so joever

--- Test.cpp
+++ Test.cpp
@@ -1,10 +1,30 @@
-#include "app.cpp"
+// #define GLFW_INCLUDE_VULKAN
+// #include <GLFW/glfw3.h>
 
-void App::run(){
-  initWindow();
-  initVulkan();
-  mainLoop();
+// #define GLM_FORCE_DEPTH_ZERO_TO_ONE
+// #include <glm/vec4.hpp>
+// #include <glm/mat4x4.hpp>
+
+// #include <iostream>
+
+int main() {
+  glfwInit();
 }
 
-// main loop for the app
-void App:mainLoop()
+glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan Window", nullptr, nullptr);
+
+uint32_t extensionCount = 0;
+vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+std::cout <<extensionCount << " extensions supported\n";
+
+glm::mat4 matrix;
+glm::vec4 vec;
+auto test = matrix * vec;
+
+while(!glfwWindowShouldClose(window)) {
+  glfwPollEvents();
+}
+
+
