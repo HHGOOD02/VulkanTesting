2b7a70069405fd057328ef187c79ea50fd1c34e7
HIGH_VIVID <henry.hawgood@gmail.com>
2024-06-15 13:46:55 -0500

    vk instance

--- main.cpp
+++ main.cpp
@@ -16,25 +16,66 @@ class HelloTriangleApplication {
 
 public:
   void run() {
+    
     initWindow();
     initVulkan();
     mainLoop();
     cleanup();
+
   }
 
 private:
   GLFWwindow* window;
 
   void initWindow() {
+
     glfwInit();
 
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
     window = glfwCreateWindow(WIDTH, HEIGHT, "Test Window", nullptr, nullptr);
+
   }
 
   void initVulkan() {
+  
+  private:
+    VkInstance instance;
+
+    createInstance();
+
+  }
+
+  void createInstance(){
+
+    VkApplicationInfo appInfo{};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Test Triangle";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+    VkInstanceCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+    uint32_t glfwExtensionCount = 0;
+    const char** glfwExtensions;
+
+    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+    createInfo.enabledExtensionCount = glfwExtensionCount;
+    createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+    createInfo.enabledLayerCount = 0;
+
+    VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+
+    if(vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+      throw std::runtime_error("failed to create instance");
+    }
 
   }
 
@@ -45,9 +86,11 @@ private:
   }
 
   void cleanup() {
+
     glfwDestroyWindow(window);
 
     glfwTerminate();
+   
   }
 
 };
